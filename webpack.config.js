bd1a601c6f1b6aa76fcf3816b341b451b3458ba2
erick <erick@hotmail.ca>
2017-11-27 19:35:36 -0500

    🎉 Add basic config files

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/js/app.js','./src/style/main.scss'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'minify']
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'postcss-loader', 'sass-loader'])
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw-loader'
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'style.bundle.css',
+      allChunks: true,
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html', // Output
+      template: './src/index.html' // Input
+    }),
+    new CopyWebpackPlugin([{
+      from:'./src/images/*',
+      to:'./images/[name].[ext]'
+    }])
+  ],
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    watchContentBase: true,
+    port: 9000
+  }
+};
