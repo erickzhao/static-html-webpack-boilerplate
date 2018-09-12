3ba674440ebfdbc5bf3b0d742afda907504cd78a
Leonardo Merza <ljmerza@gmail.com>
2018-09-12 14:35:43 -0400

    fixed html reloading

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,5 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const path = require('path');
-
 const common = require('./webpack.config.common.js');
 
 module.exports = merge(common, {
