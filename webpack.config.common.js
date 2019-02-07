bc327f5ef8506102fa0e6567318890efd0c6cbf5
Leonardo Merza <ljmerza@gmail.com>
2019-02-07 11:05:54 -0500

    added fonts to file-loader

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -35,9 +35,21 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
-            options: {},
+            options: {
+              outputPath: 'static/'
+            },
           },
         ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
       },
     ],
   },
